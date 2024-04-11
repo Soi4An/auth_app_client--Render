c3b82010847a81554d5ce1865b2e8a21e6a82f65
Antonio S <soi4.toxa@ukr.net>
2024-04-11 16:35:37 +0300

    changed Router type

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import { HashRouter as Router } from 'react-router-dom';
+// import { HashRouter as Router } from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 import { Provider } from 'react-redux';
 import { store } from './utils/redux/store';
